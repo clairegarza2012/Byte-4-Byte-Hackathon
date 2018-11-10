133208ca6ece05c0e7bff9da138f86be70c326b1
bussdieker <adam_bussdieker@hotmail.com>
2018-11-10 13:33:58 -0700

    6y55

---
+++ byte4bite-hackathon/Scripts/Custom/inventory.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+
+    $("#inventoryTemplate").click(function () {
+        $.post("/Inventory/Index", function (data) {
+            var source = $(".dummytemplate").html();
+            var template = Handlebars.compile(source);
+            $("#inventoryHolder").empty().append(template(data));
+        });
+    });
+
+});
